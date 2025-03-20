278625853d5f55fad239956abaa00253562de957
dnovak_home <dnovak@student.42.fr>
2025-03-20 14:28:41 +0100

    fix: segmentation fault when minishell started with empty environment and tried to modify it

--- src/environ/environ.c
+++ src/environ/environ.c
@@ -6,7 +6,7 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 22:43:15 by dnovak            #+#    #+#             */
-/*   Updated: 2025/02/04 14:30:37 by dnovak           ###   ########.fr       */
+/*   Updated: 2025/03/20 10:41:34 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,6 @@ t_status	setup_envp(t_envp *dest, t_envp orig)
 	int	envp_count;
 
 	envp_count = envp_size(orig);
-	if (envp_count == 0)
-	{
-		*dest = NULL;
-		return (SUCCESS);
-	}
 	*dest = ft_calloc(envp_count + 1, sizeof(char *));
 	if (*dest == NULL)
 		return (FAILURE);
